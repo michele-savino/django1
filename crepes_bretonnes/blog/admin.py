20b25a290182ce8846d0d57a4a7b2fa335787d52
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-04-19 00:41:13 +0200

    Initial commit

---
+++ crepes_bretonnes/blog/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.utils.text import Truncator
+
+from blog.models import Categorie, Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display   = ('titre', 'categorie', 'auteur', 'date', 'apercu_contenu')
+    list_filter    = ('auteur','categorie', )
+    date_hierarchy = 'date'
+    ordering       = ('date', )
+    search_fields  = ('titre', 'contenu')
+    prepopulated_fields = {'slug': ('titre',), }
+
+    # Configuration du formulaire d'édition
+    fieldsets = (
+        # Fieldset 1 : meta-info (titre, auteur…)
+        ('Général', {
+            'classes': ['collapse', ],
+            'fields': ('titre', 'slug', 'auteur', 'categorie')
+        }),
+        # Fieldset 2 : contenu de l'article
+        ('Contenu de l\'article', {
+            'description': 'Le formulaire accepte les balises HTML. Utilisez-les à bon escient !',
+            'fields': ('contenu',)
+        }),
+    )
+
+    def apercu_contenu(self, article):
+        """
+        Retourne les 40 premiers caractères du contenu de l'article,
+        suivi de points de suspension si le texte est plus long.
+        """
+        return Truncator(article.contenu).chars(40, truncate='...')
+
+    # En-tête de notre colonne
+    apercu_contenu.short_description = 'Aperçu du contenu'
+
+admin.site.register(Categorie)
+admin.site.register(Article, ArticleAdmin)
---
+++ crepes_bretonnes/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('accueil', views.home),
+    #path('article/<int:id_article>', views.view_article),
+    #path('articles/<str:tag>', views.list_articles_by_tag),
+    #path('articles/<int:year>/<int:month>', views.list_articles),
+    path('redirection', views.view_redirection),
+    path('date', views.date_actuelle),
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition),
+    path('', views.accueil, name='accueil'),
+    #path('article/<int:id>', views.lire, name='lire'),
+    path('article/<int:id>-<slug:slug>$', views.lire, name='lire'),
+]
